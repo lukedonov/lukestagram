afaf71c07883904d63b8d768eeb0d290498fc080
Luke Donovan <lukelamnisos@icloud.com>
2020-04-12 15:47:07 +0100

    can add images

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -15,6 +15,6 @@ class PostsController < ApplicationController
     private
   
     def post_params
-      params.require(:post).permit(:caption, :user_id)
+      params.require(:post).permit(:caption, :user_id, :image)
     end
   end
